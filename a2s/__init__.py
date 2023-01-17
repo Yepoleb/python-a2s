c761c2974736b168a30de7c77c5d57871f855131
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2023-01-17 04:38:06 +0000

    fix `__init__` imports

--- a2s/__init__.py
+++ a2s/__init__.py
@@ -1,4 +1,18 @@
-from a2s.exceptions import *
-from a2s.info import *
-from a2s.players import *
-from a2s.rules import *
+__all__ = (
+    "BrokenMessageError",
+    "BufferExhaustedError",
+    "SourceInfo",
+    "GoldSrcInfo",
+    "Player",
+    "info",
+    "ainfo",
+    "players",
+    "aplayers",
+    "rules",
+    "arules",
+)
+
+from a2s.exceptions import BrokenMessageError, BufferExhaustedError
+from a2s.info import GoldSrcInfo, SourceInfo, ainfo, info
+from a2s.players import Player, aplayers, players
+from a2s.rules import arules, rules
