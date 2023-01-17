9d8c37eaf5f494e2c7265b67d60a56b513a3dca1
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2023-01-17 16:26:11 +0000

    fix wrong type in base  class

--- a2s/a2s_protocol.py
+++ a2s/a2s_protocol.py
@@ -9,14 +9,14 @@ from .byteio import ByteReader
 class A2SProtocol:
     @staticmethod
     def serialize_request(challenge: int) -> bytes:
-        raise NotImplemented
+        raise NotImplementedError()
 
     @staticmethod
     def validate_response_type(response_type: int) -> bool:
-        raise NotImplemented
+        raise NotImplementedError()
 
     @staticmethod
     def deserialize_response(
         reader: ByteReader, response_type: int, ping: Optional[float]
     ) -> Any:
-        raise NotImplemented
+        raise NotImplementedError()
